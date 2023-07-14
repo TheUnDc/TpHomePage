b616aacfc4836c1fadef38fa1c66a7e0b193a4e5
Tomasz Pietruczuk <tomekpietruczuk3@gmail.com>
2023-07-14 13:32:37 +0200

    Fixed code naming convention and separated bigger functions to multiple smaller

--- js/script.js
+++ js/script.js
@@ -1,87 +1,96 @@
 {
-    const greetings = () => {
+    const doDevGreetings = () => {
         console.log("Greetings! You must be crazy to come here :P");
     };
 
     const blockadeButtonElement = document.querySelector(".js-blockadeButton");
-    const blockadeResponse = document.querySelector(".js-blockadeResponse");
+    const responseSpecialFunction = document.querySelector(".js-specialFunctionResponse");
     const blockadeEditable = document.getElementById("idBlockadeButton");
+    const hellbuttonElement = document.querySelector(".js-hellButton");
     
 
-    const blockadeSpammer = () => {
+    const scrollSpecialFunction = () => {
+        document.body.scrollTop = 0; // INFO: scroll for Safari
+        document.documentElement.scrollTop = 0; //INFO: scroll for rest
+    };
+
+    const reloadSpecialFunction = () => {
+        scrollSpecialFunction();
+        location.reload(true);
+    };
+
+    const breakSpecialFunction = () => {
+        blockadeButtonElement.removeEventListener("click", doBlockadeSpecialFunction);
+        hellbuttonElement.removeEventListener("click", doHellSpecialFunction);
+        blockadeButtonElement.addEventListener("click", reloadSpecialFunction);
+        hellbuttonElement.addEventListener("click", reloadSpecialFunction);
+    };
+
+    const doBlockadeSpecialFunction = () => {
+
         if (blockadeEditable.value === "") {
-            blockadeResponse.innerText = "Napisz co o tym myślisz: - ale napisz coś! Nie bądź cham >:("
+            responseSpecialFunction.innerText = "🤔Napisz co o tym myślisz: - ale napisz coś! Nie bądź cham >:(";
 
         } else {
-            const blockadeSpamElement = document.querySelector(".js-blockadeSpam");
-            const naviElement = document.getElementById("navi"); //zadeklarowalem  ten sam element tu i w funkcji hell
-
-            blockadeButtonElement.innerText = "Zostałeś zablokowany za posiadanie opini niezgodnej z linią mojej filozofii. Wciśnij by się rozgrzeszyć!"
-            blockadeResponse.innerText = "Jak śmiesz mieć opinię! Nie ma znaczenia, co o tym myślisz!"
-            blockadeSpamElement.classList.add("specialFunction__blockade");
-            naviElement.style.display = "none"
-
-            const textElement = "Zablokowano";
-            const multiplierX = textElement.repeat(3);
-            const multiplierY = `${multiplierX}<br>`
-            const IImultiplier = multiplierY.repeat(30);
-
-            blockadeSpamElement.innerHTML = IImultiplier
-
-            document.body.scrollTop = 0; // INFO: scroll for Safari
-            document.documentElement.scrollTop = 0; //INFO: scroll for rest
-
-            blockadeButtonElement.removeEventListener("click", blockadeSpammer);
-            hellbuttonElement.removeEventListener("click", hell);
-            blockadeButtonElement.addEventListener("click", absolution);
-            hellbuttonElement.addEventListener("click", absolution);
+            modifyBlockadeContents();
+            spamBlockadeText();
+            scrollSpecialFunction();
+            breakSpecialFunction();
         }
     };
 
-    const hellbuttonElement = document.querySelector(".js-hellButton");
-    
-    const hell = () => {
-        blockadeResponse.innerText = "Ta wersja strony powstała przy współudziale 💧︎⌘︎♋︎⧫︎♋︎■︎♋︎. Witaj w piekle!";
-        hellbuttonElement.innerText = "Zostałeś zesłany do Tartaru za tworzenie stron rodem z 2010. Wciśnij by się rozgrzeszyć!";
+    const spamBlockadeText = () => {
+        const blockadeSpamElement = document.querySelector(".js-blockadeSpam");
+        const textElement = "Zablokowano";
+        const multiplierX = textElement.repeat(3);
+        const multiplierY = `${multiplierX}<br>`;
+        const IImultiplier = multiplierY.repeat(30);
+        blockadeSpamElement.innerHTML = IImultiplier;
+    };
 
-        const miniPortrait = document.querySelector(".section__miniImage");
-        const portrait = document.getElementById("portrait");
-        const mainHeader = document.getElementById("mainHeader");
-        const naviElement = document.getElementById("navi"); //zadeklarowalem ten sam element tu i w funkcji blockadespammer
+    const modifyBlockadeContents = () => {
+        const blockadeSpamElement = document.querySelector(".js-blockadeSpam");
+        const naviElement = document.getElementById("navi");
+        
+        blockadeButtonElement.innerText = "❗Zostałeś zablokowany za posiadanie opini niezgodnej z linią mojej filozofii.❗ 👉Wciśnij by się rozgrzeszyć!👈";
+        responseSpecialFunction.innerText = "❗Jak śmiesz mieć opinię!❗ ❗Nie ma znaczenia, co o tym myślisz!❗";
+        blockadeSpamElement.classList.add("specialFunction__blockade");
+        naviElement.style.display = "none";
+    };
 
-        portrait.style.border = "0"
-        miniPortrait.style.border = "0"
-        mainHeader.style.border = "0"
-        naviElement.style.display = "none"
+    const doHellSpecialFunction = () => {
 
-        blockadeEditable.style.backgroundColor = "#9a0b0b";
-        blockadeResponse.style.fontFamily = "Roboto Condensed";
-        hellbuttonElement.style.fontFamily = "Roboto Condensed";
+        modifyHellContents();
+        modifyHellStyles();
+        scrollSpecialFunction();
+        breakSpecialFunction();
+    }
 
+    const modifyHellStyles = () => {
         const containerElement = document.querySelector(".container__bodyStyle");
         const body = document.body;
-
+        
         blockadeButtonElement.classList.add("specialFunction__hell");
         containerElement.classList.add("specialFunction__hell");
         body.classList.add("container__body--hell");
+        blockadeEditable.style.backgroundColor = "#9a0b0b";
+        responseSpecialFunction.style.fontFamily = "Roboto Condensed";
+        hellbuttonElement.style.fontFamily = "Roboto Condensed";
+    };
 
+    const modifyHellContents = () => {
+        const miniPortrait = document.querySelector(".section__miniImage");
+        const portrait = document.getElementById("portrait");
+        const naviElement = document.getElementById("navi");
+
+        responseSpecialFunction.innerText = "😈Ta wersja strony powstała przy współudziale 💧︎⌘︎♋︎⧫︎♋︎■︎♋︎.😈 😈Witaj w piekle!😈";
+        hellbuttonElement.innerText = "💔Zostałeś zesłany do Tartaru za tworzenie stron rodem z 2010.💔 👉Wciśnij by się rozgrzeszyć!👈";
         portrait.src = "../images/Devil2.jpg";
         miniPortrait.src = "../images/hell_mason.jpg";
+        naviElement.style.display = "none";
+    };
 
-        document.body.scrollTop = 0; //INFO: scroll for Safari
-        document.documentElement.scrollTop = 0; //INFO: scroll for rest
-
-        blockadeButtonElement.removeEventListener("click", blockadeSpammer);
-        hellbuttonElement.removeEventListener("click", hell);
-        blockadeButtonElement.addEventListener("click", absolutionReload);
-        hellbuttonElement.addEventListener("click", absolutionReload);
-    }
-
-    const absolutionReload = () => {
-        location.reload(true)
-    }
-
-    greetings()
-    blockadeButtonElement.addEventListener("click", blockadeSpammer);
-    hellbuttonElement.addEventListener("click", hell);
+    doDevGreetings()
+    blockadeButtonElement.addEventListener("click", doBlockadeSpecialFunction);
+    hellbuttonElement.addEventListener("click", doHellSpecialFunction);
 }
